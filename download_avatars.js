aebd02992edd46fb9a6369fbe8151445d0bd9187
imack5 <iainsmackenzie@gmail.com>
2018-04-16 21:47:25 +0000

    Added functionality to access and print the contributors to a project an their respective avatar URL's

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,15 @@
 var request = require('request');
+var password = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//accesses the JSON string of the contributors of a specified repo
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token'
+      'Authorization': password.GITHUB_TOKEN
     }
   };
 
@@ -16,9 +18,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
-
 getRepoContributors("jquery", "jquery", function(err, result) {
+  var contributors = JSON.parse(result);
+
+  //prints out any erros associated with accessing the information
   console.log("Errors:", err);
-  console.log("Result:", result);
+
+  //prints out all of the contributers and their avatar url
+  contributors.forEach(function(element, index){
+    console.log('#' + (index + 1), '\b:' + element.login, '--', element.avatar_url );
+
+  });
+  //console.log("Result:", contributors);
 });
